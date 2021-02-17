d57b66f637204633750544941137f85a2fff0c3d
Gabriel Miranda <ggmirandc@gmail.com>
2021-02-17 03:25:38 -0300

    finish of exercise

--- Bioinformatics_codes/FrequentWordWithMismatches.py
+++ Bioinformatics_codes/FrequentWordWithMismatches.py
@@ -1,8 +1,7 @@
 import os
 repo=os.getcwd()
 
-import os
-repo=os.getcwd()
+
 
 def HammingDistance(p,q):
     counter=0
@@ -28,4 +27,38 @@ def Neighbors(Pattern,d):
     return Neighborhood
 
 def FrequentWordWithMismatches(Text,k,d):
-    pass
+    Patterns=[]
+    freqMap={}
+    n=len(Text)
+    if n==k:
+        ran=1
+    else:
+        ran=n-k
+    for i in range(ran):
+        Pattern=Text[i:i+k]
+        Neighborhood=list(Neighbors(Pattern,d))
+        limi=len(Neighborhood)-1
+        if limi==0:
+            limi=1
+        for j in range(limi):
+            neighbor=Neighborhood[j]
+            try:
+                freqMap[neighbor]=freqMap[neighbor]+1
+            except KeyError:
+                freqMap[neighbor]=1
+    m=freqMap[max(freqMap, key=lambda k:freqMap[k])]
+    for  k in freqMap:
+        if freqMap[k]==m:
+            Patterns.append(k)
+    return Patterns
+
+re=open(repo+"/texts/fwm.txt",'r')
+fwm=re.readlines()
+re.close
+for i in range(len(fwm)):
+    fwm[i]=fwm[i].strip()
+
+text=fwm[0]
+k=int(fwm[1][0])
+d=int(fwm[1][2])
+print("FWWM:",*FrequentWordWithMismatches(text,k,d))
