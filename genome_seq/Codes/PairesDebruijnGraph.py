537caf2ed5303728181ca6540893ff128921ad6c
Gabriel Miranda <ggmirandc@gmail.com>
2021-03-04 17:36:25 -0300

    changes in paired deb

--- genome_seq/Codes/PairesDebruijnGraph.py
+++ genome_seq/Codes/PairesDebruijnGraph.py
@@ -1,36 +1,27 @@
 from collections import defaultdict
-def Composition(k,dna_string):
-    comp=[]
-    for i in range(len(dna_string)-k+1):
-        kmer=dna_string[i:i+k]
-        comp.append(kmer)
-    return comp
-
-def PairedComposition(k,d,string):
-    pairs=[]
-    comp=Composition(k,string)
-    for i in range(len(comp)-(k+d)):
-        par=comp[i]+"|"+comp[i+k+d]
-        pairs.append(par)
-    return pairs
-
 
 def PrefixPair(pair):
     pares=pair.split("|")
-    return [pares[0][0:len(pares[0])-1],pares[1][0:len(pares[1])-1]]
+    return pares[0][0:len(pares[0])-1]+"|"+pares[1][0:len(pares[1])-1]
 
 def SuffixPair(pair):
     pares=pair.split("|")
-    return [pares[0][1:],pares[1][1:]]
+    return pares[0][1:]+"|"+pares[1][1:]
 
-def PairedDebruijnGraph(k,d,Text):
-    pares=PairedComposition(k,d,Text)
-    conection_dic={}
-    for par in pares:
-        conection_dic[par]=[]
-        for par2 in pares:
-            if par!=par2 and SuffixPair(par)==PrefixPair(par2):
-                conection_dic[par].append(par2)
-    return conection_dic
+def PairedCompositionGraph(kdmers):
+    dic_com=defaultdict(list)
+    for elem in kdmers:
+        nodes=[PrefixPair(elem),SuffixPair(elem)]
+        dic_com[elem].append(nodes)
+    return dic_com
 
-print(PairedDebruijnGraph(3,1,"TAATGCCATGGGATGTT"))
+
+def PairedDebruijnGraph(k,d,kdmers):
+    conections_dic=defaultdict(list)
+    comp=PairedCompositionGraph(kdmers)
+    for key in comp:
+        for nodes in comp[key]:
+            conections_dic[nodes[0]].append(nodes[1])
+    return conections_dic
+
+print(PairedDebruijnGraph(3,1,["AAT|CCA","ATG|CAT","ATG|GAT","CAT|GGA","CCA|GGG","GCC|TGG","GGA|GTT","GGG|TGT","TAA|GCC","TGC|ATG","TGG|ATG"]))
