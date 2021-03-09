eb2d7180b25312fedb55131b0b064c89bafd70fe
Gabriel Miranda <ggmirandc@gmail.com>
2021-03-09 02:25:15 -0300

    trying to do linear spectrum

--- genome_seq/Codes/GeneratingTheoricalSpectrum.py
+++ genome_seq/Codes/GeneratingTheoricalSpectrum.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 repo=os.getcwd()
 def GenerateMassTable():
     with open(repo+"/genome_seq/codes/integer_mass_table.txt","r") as reader:
@@ -6,6 +7,22 @@ def GenerateMassTable():
         pairs=list(map(str.split,inp))
         pairs=list(map(tuple,pairs))
     pairs_dic=dict(pairs)
+    #print("".join([key for key in pairs_dic]))
     return pairs_dic
 
-print(GenerateMassTable())
+def LinearSpectrum(Peptide,alphabet="GASPVTCILNDKQEMHFRYW",AminoAcidMass=GenerateMassTable()):
+    PrefixMass=defaultdict(int)
+    PrefixMass[0]=0
+    #print(PrefixMass[0])
+    for i in range(len(Peptide)):
+        for j in alphabet:
+            if j==Peptide[i]:
+                #print(PrefixMass[i-1])
+                #print(int(AminoAcidMass[j]))
+                PrefixMass[i]=PrefixMass[i-1]+int(AminoAcidMass[j])
+    linear_spectrum=[0]
+    for x in range(len(Peptide)):
+        for y in range(i+1,len(Peptide)+1):
+            linear_spectrum.append(PrefixMass[x]-PrefixMass[y])
+    return linear_spectrum
+print(LinearSpectrum("NQEL"))
