10277c7ce87514b3fd7cd2683ff8b0c50ce86e24
Gabriel Miranda <ggmirandc@gmail.com>
2021-03-20 20:53:49 -0300

    agregar __name__=="__main__"

--- genome_seq/Codes/PathToGenome.py
+++ genome_seq/Codes/PathToGenome.py
@@ -9,10 +9,10 @@ def PathToGenome(path):
         else:
             genome+=elem[k-1]
     return genome
-
-with open(repo+"/genome_seq/inputs/pathgeno.txt","r") as reader:
-    pathgeno=list(map(str.strip,reader.readlines()))
-    path=pathgeno
-ptog=PathToGenome(path)
-with open(repo+"/genome_seq/outputs/pathgeno_solve.txt","w") as writter:
-    writter.write(ptog)
+if __name__=="__main__":
+    with open(repo+"/genome_seq/inputs/pathgeno.txt","r") as reader:
+        pathgeno=list(map(str.strip,reader.readlines()))
+        path=pathgeno
+    ptog=PathToGenome(path)
+    with open(repo+"/genome_seq/outputs/pathgeno_solve.txt","w") as writter:
+        writter.write(ptog)
